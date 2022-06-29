c05612cea6e3506d7c5c9c33d96673d92348de53
Mason Tran <Mason.Tran@silabs.com>
2022-06-29 13:14:40 -0400

    Only call sl_power_manager_sleep() in baremetal environment

--- src/src/system.c
+++ src/src/system.c
@@ -155,7 +155,7 @@ void otSysProcessDrivers(otInstance *aInstance)
     // See alarm.c: Wrapped in a critical section
     efr32AlarmProcess(aInstance);
 
-#if defined(SL_CATALOG_POWER_MANAGER_PRESENT)
+#if defined(SL_CATALOG_POWER_MANAGER_PRESENT) && !defined(SL_CATALOG_KERNEL_PRESENT)
     // Let the CPU go to sleep if the system allows it.
     sl_power_manager_sleep();
 #endif
