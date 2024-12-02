82a6ea7382bf85fa9436eacc971464be50b85ea5
Jonathan Hui <jonhui@google.com>
2024-12-02 15:24:03 -0800

    examples: `OT_NETIF_THREAD` -> `OT_NETIF_THREAD_INTERNAL` (#1024)
    * submodule: bump openthread from `ae5b750` to `473af53`

--- examples/sleepy-demo/sleepy-mtd.c
+++ examples/sleepy-demo/sleepy-mtd.c
@@ -175,7 +175,7 @@ void initUdp(void)
     }
 
     // Bind to the socket. Close the socket if bind fails.
-    error = otUdpBind(otGetInstance(), &sMtdSocket, &bindAddr, OT_NETIF_THREAD);
+    error = otUdpBind(otGetInstance(), &sMtdSocket, &bindAddr, OT_NETIF_THREAD_INTERNAL);
     if (error != OT_ERROR_NONE)
     {
         otCliOutputFormat("MTD failed to bind udp socket with: %d, %s\r\n", error, otThreadErrorToString(error));
